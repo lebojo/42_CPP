b523a79d2cdd72e259ec5c75b5583666f739d819
LeBojo <contact@jordan-chap.com>
2023-12-14 22:31:27 +0000

    CPP03_ex01_done

--- CPP03/ex01/ClapTrap.cpp
+++ CPP03/ex01/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: lebojo <lebojo@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/14 05:12:14 by jchapell          #+#    #+#             */
-/*   Updated: 2023/12/14 22:05:00 by lebojo           ###   ########.fr       */
+/*   Updated: 2023/12/14 22:22:17 by lebojo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,33 +22,51 @@ void	print(string str)
 	std::cout << str;
 }
 
+void color()
+{
+	std::cout << "\e[0;35m";
+}
+
+void reset()
+{
+	std::cout << "\033[0m";
+}
+
 ClapTrap::ClapTrap(string newName)
 {
+	color();
 	print(newName);
+	reset();
 	println(" just spawned!");
 	this->name = newName;
-	this->healthPoint = 10;
-	this->energyPoint = 10;
-	this->attackDamage = 0;
+	this->healthPoint = 100;
+	this->energyPoint = 50;
+	this->attackDamage = 20;
 }
 
 ClapTrap::~ClapTrap()
 {
+	color();
 	print(this->name);
+	reset();
 	println(" is broken");
 }
 
 void ClapTrap::attack(const string& target)
 {
 	if (this->healthPoint <= 0) return;
+	color();
 	print(this->name);
+	reset();
 	if (this->energyPoint <= 0)
 	{
 		println(" is exhausted");
 		return;
 	}
 	print(" attack ");
+	color();
 	print(target);
+	reset();
 	print(" causing ");
 	std::cout << this->attackDamage;
 	println(" points of damage!");
@@ -58,7 +76,9 @@ void ClapTrap::attack(const string& target)
 void ClapTrap::takeDamage(unsigned int amount)
 {
 	if (this->healthPoint <= 0) return;
+	color();
 	print(this->name);
+	reset();
 	print(" take ");
 	std::cout << amount;
 	print(" points of damage :o (");
@@ -67,7 +87,9 @@ void ClapTrap::takeDamage(unsigned int amount)
 	println("HP remain)");
 	if ( this->healthPoint <= 0)
 	{
+		color();
 		print(this->name);
+		reset();
 		println(" can't hold it anymore...");
 		this->healthPoint = 0;
 	}
@@ -75,7 +97,9 @@ void ClapTrap::takeDamage(unsigned int amount)
 
 void ClapTrap::beRepaired(unsigned int amount)
 {
+	color();
 	print(this->name);
+	reset();
 	print(" is healed of ");
 	std::cout << amount;
 	print(" HP!(");
---
+++ CPP03/ex01/ScavTrap.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lebojo <lebojo@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/14 21:43:19 by lebojo            #+#    #+#             */
+/*   Updated: 2023/12/14 22:28:06 by lebojo           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+# include "ClapTrap.hpp"
+
+class ScavTrap : public ClapTrap
+{
+public:
+	ScavTrap(string newName);
+	~ScavTrap();
+	
+	void guardGate();
+};
+
+#endif
