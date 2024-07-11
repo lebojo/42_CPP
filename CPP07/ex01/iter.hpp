206890b6f8b8efe0c0657d2cffabc749d0857c90
Jordan Chapelle <jchapell@c3r3s5.42lausanne.ch>
2024-07-11 12:23:08 +0200

    Corrected cpp07 ex01

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,8 +1,8 @@
 #ifndef iter_HPP
 # define iter_HPP
 
-template <typename T>
-void iter(T* arr, int len, void (*fnc)(T))
+template <typename T, typename Func>
+void iter(T* arr, int len, Func fnc)
 {
 	for(int i = 0; i < len; i++)
 		fnc(arr[i]);
